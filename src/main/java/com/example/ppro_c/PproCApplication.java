c299ee636ba3ae194f4059ffec7fe0cecd4d0eb4
pallado1 <dominik.palla@uhk.cz>
2024-10-02 14:45:12 +0200

    Initial commit

---
+++ src/main/java/com/example/ppro_c/PproCApplication.java
@@ -0,0 +1,13 @@
+package com.example.ppro_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PproCApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PproCApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ppro_c/PproCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ppro_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PproCApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
